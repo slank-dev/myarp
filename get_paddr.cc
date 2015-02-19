9572f1a581a73245541ba223b0dd22042729b72b
slank-lin <slank.dev@gmail.com>
2015-02-19 03:08:05 -0800

    test

--- get_paddr.cc
+++ get_paddr.cc
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>		//for close()
+#include <net/if.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
 
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h> 	//for close()
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
@@ -35,7 +43,42 @@ char* get_paddr(const char* ifname){
 	return ipstr;
 }
 
+
+
+
+u_char* get_haddr( const char* ifname){
+	static u_char *haddr;
+	int sockd;
+	struct ifreq ifr;	//<net/if.h>
+
+	haddr = (u_char*)malloc(sizeof(u_char)*6);
+
+	if((sockd=socket(AF_INET,SOCK_DGRAM,0)) < 0){
+		perror("socket()!");
+		exit(-1);
+	}
+
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1);
+	
+	ioctl(sockd, SIOCGIFHWADDR, &ifr);
+	close(sockd);
+	
+	for(int i=0; i<6; i++)	haddr[i] = (unsigned char)ifr.ifr_hwaddr.sa_data[i];
+
+	return haddr;
+}
+
+
+
+
 int main(){
-	char* str = get_paddr("wlan0");
+	char* str = get_paddr("eth0");
 	printf("%s\n", str);
+	
+	u_char* strs = get_haddr("eth0");
+	for(int i=0; i<6; i++)	printf("%02X:", strs[i]);
+	printf("\n");
+
+	return 0;
 }
