d947465ee8c22b093531d057e65a779680c53198
slank-lin <slank.dev@gmail.com>
2015-02-25 10:58:23 +0900

    version 0.00 beta

--- arp.cc
+++ arp.cc
@@ -3,8 +3,6 @@
  *
  *	Copyright (C) 2014-2015 Hiroki Shirokura <mail: slank.dev@gmail.com>
  *
- *	This file is part of this program .
- *
  *	PROGRAMNAME is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation, either version 3 of the License, or
