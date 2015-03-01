53ac221886ffde37f5e5a574a94931cc54634c7f
slank-lin <slank.dev@gmail.com>
2015-03-01 15:24:29 +0900

    version 0.10 2015.3.1

--- slank.h
+++ slank.h
@@ -1,25 +1,24 @@
+
 /*
+ *Copyright (C) 2014-2015 Hiroki Shirokura <mail: slank.dev@gmail.com>
  *
- *	Copyright (C) 2014-2015 Hiroki Shirokura <mail: slank.dev@gmail.com>
+ *TLexScan is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
  *
- *	PROGRAMNAME is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ *TLexScan is distributed in the hope that it will be useful,
  *
- *	This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
  *
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program.  
- *	If not, see <http://slankdev.wordpress.com>.
- *	slank (Hiroki Shirokura) <mail: slank.dev@gmail.com>
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  
+ *If not, see <http://slankdev.wordpress.com>.
+ *slank (Hiroki Shirokura) <mail: slank.dev@gmail.com>
  *
  */
-
 
 #include <stdio.h>
 #include <string.h>
