feb6d9316dfc9feea360895d1fe7753b09c0d248
slank-lin <slank.dev@gmail.com>
2015-03-16 10:49:32 +0900

    fix

--- capLan.cc
+++ capLan.cc
@@ -1,4 +1,27 @@
 
+/*
+ *Copyright (C) 2014-2015 Hiroki Shirokura <mail: slank.dev@gmail.com>
+ *
+ *TLexScan is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *TLexScan is distributed in the hope that it will be useful,
+ *
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  
+ *If not, see <http://slankdev.wordpress.com>.
+ *slank (Hiroki Shirokura) <mail: slank.dev@gmail.com>
+ *
+ */
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -17,6 +40,9 @@
 #include "myclass.h"
 #include "util.h"
 #include "debug.h"
+
+
+
 
 void CaptureCallback(u_char *data, const struct pcap_pkthdr *header,
 										const u_char* packet){
@@ -69,9 +95,9 @@ void CaptureCallback(u_char *data, const struct pcap_pkthdr *header,
 
 
 
-int CaptureLan(TLexOps opt){
 
-	printf("longcapture lan!!\n");
+
+int CaptureLan(TLexOps opt){
 	
 	int space = 60;
 	int addr_count;
@@ -114,4 +140,6 @@ int CaptureLan(TLexOps opt){
 	pcap_close(handle);
 	return 1; 
 }
+
+
 
